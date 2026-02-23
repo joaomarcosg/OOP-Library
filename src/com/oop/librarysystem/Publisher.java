ecda608029ac5fc8bdc5c67d4c82a498e58cf739
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-02-23 12:12:00 -0300

    FEAT: adds Publisher class

---
+++ src/com/oop/librarysystem/Publisher.java
@@ -0,0 +1,31 @@
+package com.oop.librarysystem;
+
+public class Publisher {
+    private String fantasyName;
+    private String corporateReason;
+    private String cnpj;
+
+    public String getFantasyName() {
+        return fantasyName;
+    }
+
+    public void setFantasyName(String fantasyName) {
+        this.fantasyName = fantasyName;
+    }
+
+    public String getCorporateReason() {
+        return corporateReason;
+    }
+
+    public void setCorporateReason(String corporateReason) {
+        this.corporateReason = corporateReason;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+}
